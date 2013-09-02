c6e1db2e91e1e6fbc2c1ad1536e82e1edcb7ee85
Matthieu Bontemps <matthieu.bontemps@knplabs.com>
2013-09-02 11:11:47 +0200

    Use default site

--- .playwithme.sh
+++ .playwithme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt-get install -y --force-yes apache2
-cp .playwithme/virtual-host.conf /etc/apache2/sites-available/demo.conf
-a2ensite demo
-service apache2 reload
+sudo apt-get install -y --force-yes apache2
+sudo a2enmod rewrite
+cp .playwithme/virtual-host.conf /etc/apache2/sites-available/default.conf
+sudo a2ensite default
+sudo service apache2 reload
