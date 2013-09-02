cb3d1f091aabe5f9e55bc4b4605e85ba1f634e40
Matthieu Bontemps <matthieu.bontemps@knplabs.com>
2013-09-02 12:05:19 +0200

    Fix path

--- .playwithme.sh
+++ .playwithme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y --force-yes apache2
 sudo a2enmod rewrite
-sudo cp .playwithme/virtual-host.conf /etc/apache2/sites-available/default.conf
+sudo cp .playwithme/virtual-host.conf /etc/apache2/sites-available/default
 sudo a2ensite default
 sudo service apache2 reload
